d0a52a4a73691aebbbffe6f44e974c3f67fa5ccb
Devansh Sharma <kdevanshsharma23@gmail.com>
2021-03-20 14:06:07 +0530

    feat : loc added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,23 @@
 import Navbar from "../components/Navbar/Navbar";
 import Hero from "../components/Hero/Hero";
 import HouseCard from "../components/HouseCard/HouseCard";
+import React from "react"
+var ip = require('ip');
+const ipLocation = require("iplocation");
+
+
 const Home = () => {
+  const [loc, setLoc] = React.useState("")
+
+  const updateLoc = async () => {
+    const myIp = ip.address()
+    console.log(myIp)
+    const loc = await ipLocation(myIp);
+    console.log(loc)
+  }
+  React.useEffect(() => {
+    updateLoc()
+  }, [])
   return (
     <div>
       <Navbar />
