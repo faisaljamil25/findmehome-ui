8489a73f3c7a22e07719be354eb6a9dee73b06bf
Faisal Jamil <faisaljamilcs@gmail.com>
2021-03-20 13:13:38 +0530

    feat: routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import SignIn from "./components/SignIn/SignIn";
+import SignUp from "./components/SignUp/SignUp";
+
+import "./App.css";
 
 const App = () => {
-  return <Home />;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/signin" exact component={SignIn} />
+          <Route path="/signup" exact component={SignUp} />
+        </Switch>
+      </Router>
+    </>
+  );
 };
 
 export default App;
