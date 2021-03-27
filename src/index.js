926e31335780070cb557ca1958041662ea0ef9bf
Faisal Jamil <faisaljamilcs@gmail.com>
2021-03-27 23:42:02 +0530

    fix: remove unused package

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import reducers from "./reducers";
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,21 +3,7 @@ import Hero from "../components/Hero/Hero";
 import React from "react";
 import Properties from "../components/Properties/Properties";
 
-var ip = require("ip");
-const ipLocation = require("iplocation");
-
 const Home = () => {
-  const [loc, setLoc] = React.useState("");
-
-  const updateLoc = async () => {
-    const myIp = ip.address();
-    console.log(myIp);
-    const loc = await ipLocation(myIp);
-    console.log(loc);
-  };
-  React.useEffect(() => {
-    updateLoc();
-  }, [loc]);
   return (
     <div>
       <Navbar />
